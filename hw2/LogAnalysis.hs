a75cdf8f964bcb6b79d53f66c6f87be42cc08788
Gabe Berke-Williams <gabebw@gabebw.com>
2014-10-23 10:20:23 -0400

    Build a binary search tree for messages

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -4,6 +4,13 @@ module LogAnalysis where
 
 import Log
 
+-- Print out some messages, ordered by timestamp
+main :: IO ()
+main = do
+  let numberOfMessages = 20
+  messages <- testParse parse numberOfMessages "error.log"
+  print $ inOrder (build messages)
+
 parseMessage :: String -> LogMessage
 parseMessage = parseMessagePieces . words
 
@@ -15,3 +22,21 @@ parseMessagePieces body = Unknown (unwords body)
 
 parse :: String -> [LogMessage]
 parse = (map parseMessage) . lines
+
+hasGreaterTimestamp :: LogMessage -> LogMessage -> Bool
+hasGreaterTimestamp (LogMessage _ ts1 _) (LogMessage _ ts2 _) = ts1 > ts2
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert m Leaf = Node Leaf m Leaf
+insert m (Node ltree msg rtree)
+  | hasGreaterTimestamp m msg = Node ltree msg (insert m rtree)
+  | otherwise = Node (insert m ltree) msg rtree
+
+build :: [LogMessage] -> MessageTree
+build = foldl (\tree m -> insert m tree) Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node Leaf m rtree) = m:(inOrder rtree)
+inOrder (Node ltree m rtree) = inOrder ltree ++ [m] ++ inOrder rtree
