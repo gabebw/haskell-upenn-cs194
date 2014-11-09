f8c7ae2c43e2368734e08b6148b33a03fe414429
Gabe Berke-Williams <gabebw@gabebw.com>
2014-11-09 10:15:03 -0500

    HW 5: 2x2 Matrix

--- fall-2014/hw5/HW05.hs
+++ fall-2014/hw5/HW05.hs
@@ -38,3 +38,63 @@ mod5RingWorks = (add (MkMod 5) (MkMod 3) == MkMod 3) &&
 mod5ParsingWorks :: Bool
 mod5ParsingWorks = (parse "3" == Just (MkMod 3, "")) &&
     (parseRing "1 + 2 * 5" == Just (MkMod 1))
+
+-- Exercise 3: Ring and Parsable instances for Mat2x2
+
+-- A row in a matrix of size 2
+data Row = Row Integer Integer deriving (Show, Eq)
+-- A 2x2 matrix has 2 rows (with each row having 2 elements)
+data Mat2x2 = Mat2x2 Row Row deriving (Show, Eq)
+
+instance Ring Mat2x2 where
+    addId = Mat2x2 (Row 0 0) (Row 0 0)
+    addInv (Mat2x2 (Row x1 y1) (Row x2 y2)) = Mat2x2 (Row (negate x1) (negate y1)) (Row (negate x2) (negate y2))
+    mulId = Mat2x2 (Row 1 0) (Row 0 1)
+    add (Mat2x2 r1 r2) (Mat2x2 r3 r4) = Mat2x2 (addRow r1 r3) (addRow r2 r4)
+    -- tl = top left, br = bottom right, etc
+    -- [ tl1 tr1 ]   [ tl2 tr2 ]   [ (tl1 * tl2 + tr1 * bl2) (tl1 * tr2 + tr1 * br2) ]
+    -- [ bl1 br1 ] x [ bl2 br2 ] = [ (bl1 * tl2 + br1 * bl2) (bl1 * tr2 + br1 * br2) ]
+    mul m1 m2 = Mat2x2 (firstRowMultiplied m1 m2) (secondRowMultiplied m1 m2)
+
+-- Parse a string like "[[1,2],[3,4]]" into Just (Mat2x2, String)
+-- The key is that parseIntoLists uses the [[Integer]] instance of `reads`.
+instance Parsable Mat2x2 where
+    parse = parse' . parseIntoLists
+
+parseIntoRow :: [[Integer]] -> Mat2x2
+parseIntoRow [[a, b], [c, d]] = Mat2x2 (Row a b) (Row c d)
+
+parse' :: Maybe ([[Integer]], String) -> Maybe (Mat2x2, String)
+parse' Nothing = Nothing
+parse' (Just (xs, s)) = Just (parseIntoRow xs, s)
+
+parseIntoLists :: String -> Maybe ([[Integer]], String)
+parseIntoLists = listToMaybe . reads
+
+-- Multiply two matrices and get the top row
+firstRowMultiplied :: Mat2x2 -> Mat2x2 -> Row
+firstRowMultiplied (Mat2x2 (Row tl1 tr1) (Row bl1 br1)) (Mat2x2 (Row tl2 tr2) (Row bl2 br2)) = Row (tl1 * tl2 + tr1 * bl2) (tl1 * tr2 + tr1 * br2)
+
+-- Multiply two matrices and get the bottom row
+secondRowMultiplied :: Mat2x2 -> Mat2x2 -> Row
+secondRowMultiplied (Mat2x2 (Row tl1 tr1) (Row bl1 br1)) (Mat2x2 (Row tl2 tr2) (Row bl2 br2)) = Row (bl1 * tl2 + br1 * bl2) (bl1 * tr2 + br1 * br2)
+
+addRow :: Row -> Row -> Row
+addRow (Row x1 y1) (Row x2 y2) = Row (x1 + x2) (y1 + y2)
+
+-- Was the Mat2x2 instance of Ring defined properly?
+mat2x2RingWorks :: Bool
+mat2x2RingWorks = (add mat1234 mat5678) == (Mat2x2 (Row 6 8) (Row 10 12)) &&
+    (add mat1234 (addInv mat1234)) == Mat2x2 (Row 0 0) (Row 0 0) &&
+    (add mat1234 addId) == mat1234 &&
+    (mul mat1234 mulId) == mat1234 &&
+    (mul mat1234 mat5678) == (Mat2x2 (Row 19 22) (Row 43 50))
+    where
+        mat1234 = Mat2x2 (Row 1 2) (Row 3 4)
+        mat5678 = Mat2x2 (Row 5 6) (Row 7 8)
+
+-- Was the Mat2x2 instance of Parsable defined properly?
+mat2x2ParsingWorks :: Bool
+mat2x2ParsingWorks = (parse "[[3,4],[5,6]]" == Just ((Mat2x2 (Row 3 4) (Row 5 6)), "")) &&
+    (parseRing "[[1,2],[3,4]] + [[5,6],[7,8]]" == Just (Mat2x2 (Row 6 8) (Row 10 12))) &&
+    (parseRing "[[1,2],[3,4]] * [[5,6],[7,8]]" == Just (Mat2x2 (Row 19 22) (Row 43 50)))
