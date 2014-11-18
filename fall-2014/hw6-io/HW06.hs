1c04695cca424200324ad881041234b684effa5b
Gabe Berke-Williams <gabebw@gabebw.com>
2014-11-18 09:35:34 -0500

    HW 6, Exercise 1

--- fall-2014/hw6-io/HW06.hs
+++ fall-2014/hw6-io/HW06.hs
@@ -8,3 +8,18 @@ import GHC.Generics
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+
+----------------------------------------------------
+-- Exercise 1: Convert (String "Y") -> (Bool True) and
+-- (String "N") -> (Bool False)
+
+ynToBool :: Value -> Value
+ynToBool (Bool True) = String "Y"
+ynToBool (Bool False) = String "N"
+ynToBool v = v
+
+ynToBoolWorks :: Bool
+ynToBoolWorks =
+    ynToBool (Bool True) == String "Y" &&
+    ynToBool (Bool False) == String "N" &&
+    ynToBool (Number 3) == Number 3
