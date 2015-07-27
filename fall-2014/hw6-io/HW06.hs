70af4990b78ac80c8646da2c0c528b43dcc4d7f4
Gabe Berke-Williams <gabebw@gabebw.com>
2015-07-26 18:18:35 -0700

    Rewrite Fall 2014 HW6 from scratch
    Homework: http://www.seas.upenn.edu/~cis194/fall14/hw/06-monoid-io.pdf
    
    Lecture: http://www.seas.upenn.edu/~cis194/fall14/lectures/06-monoid-io.html
    
    Changes:
    
    * `ynToBool` was backwards! And it didn't correctly deal with nested data
      structures, so an array of "Y" wouldn't get converted to an array of True.

--- fall-2014/hw6-io/HW06.hs
+++ fall-2014/hw6-io/HW06.hs
@@ -5,39 +5,28 @@ module HW06 where
 import Data.Aeson
 import Data.Monoid
 import GHC.Generics
+import Data.List (sort)
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import qualified Data.Vector as V
+import qualified Data.HashMap.Strict as HM
 
-import Data.List (sort)
-import Data.Maybe (listToMaybe)
-
-----------------------------------------------------
--- Exercise 1: Convert (String "Y") -> (Bool True) and
--- (String "N") -> (Bool False)
-
+-- Exercise 1: ynToBool
+-- Convert "Y" to True and "N" to False, inside aeson
 ynToBool :: Value -> Value
-ynToBool (Bool True) = String "Y"
-ynToBool (Bool False) = String "N"
+ynToBool (String "Y") = Bool True
+ynToBool (String "N") = Bool False
+ynToBool (Array a) = Array $ V.map ynToBool a
+ynToBool (Object o) = Object $ HM.map ynToBool o
 ynToBool v = v
 
-ynToBoolWorks :: Bool
-ynToBoolWorks =
-    ynToBool (Bool True) == String "Y" &&
-    ynToBool (Bool False) == String "N" &&
-    ynToBool (Number 3) == Number 3
-
-----------------------------------------------------
--- Exercise 2
--- Take in a ByteString containing JSON data and output either
--- an error message or a Value that has been processed by ynToBool.
+-- Exercise 2: parseData
+-- Take a ByteString of JSON and decode it, processing with ynToBool
 parseData :: B.ByteString -> Either String Value
 parseData = fmap ynToBool . eitherDecode
 
-----------------------------------------------------
--- Exercise 3
--- Write a Market type and parser
-
+-- Exercise 3: A Market type, and parseMarkets
 -- x is longitude
 -- y is latitude
 data Market = Market { marketname :: T.Text
@@ -49,101 +38,94 @@ data Market = Market { marketname :: T.Text
 instance FromJSON Market
 
 parseMarkets :: B.ByteString -> Either String [Market]
-parseMarkets = fmap (unwrapSuccessfulResult . fromJSON) . parseData
+parseMarkets bs = case fmap fromJSON (parseData bs) of
+                  (Left s) -> Left s
+                  (Right (Error s)) -> Left s
+                  (Right (Success a)) -> Right a
 
-unwrapSuccessfulResult :: Result a -> a
-unwrapSuccessfulResult (Success a) = a
-
-testParseMarkets :: IO ()
-testParseMarkets = do
-    fileData <- B.readFile "markets.json"
-    let name = fmap (marketname . head) $ parseMarkets fileData
-    either print print name
-
-----------------------------------------------------
--- Exercise 4
-
+-- Exercise 4: loadData to load markets.json
 loadData :: IO [Market]
-loadData = do
-    fileData <- B.readFile "markets.json"
-    -- If there's an error, fail with its message, otherwise return the parsed
-    -- data
-    either fail return $ parseMarkets fileData
+loadData = fmap (unwrapMarkets . parseMarkets) fileContents
+    where
+        fileContents = B.readFile "markets.json"
+        -- Pull out [Market], failing if we can't
+        -- And yes, `either fail id` is gross
+        unwrapMarkets :: Either String [Market] -> [Market]
+        unwrapMarkets = either fail id
 
-----------------------------------------------------
--- Exercise 5
+-- Exercise 5: Write OrdList and its Monoid instance
+-- OrdList is a wrapper around a list (like Data.Monoid.First is a wrapper
+-- around Maybe) that keeps the list ordered.
 
 data OrdList a = OrdList { getOrdList :: [a] } deriving (Eq, Show)
 
 instance (Ord a) => Monoid (OrdList a) where
-    mappend x xs = OrdList $ sort $ (getOrdList x) ++ (getOrdList xs)
+    mappend (OrdList a) (OrdList b) = OrdList $ sort (a ++ b)
     mempty = OrdList []
 
-evens :: OrdList Integer
-evens = OrdList [2,4,6]
+-- Does the OrdList monoid work?
+ordListWorks =
+    (evens <> odds) == (OrdList [1..6]) &&
+    (evens <> odds) == (odds <> evens) &&
+    (evens <> mempty) == evens &&
+    (odds <> mempty) == odds
+    where
+        combinedEvensOdds = evens <> odds
+        combinedOddsEvens = evens <> odds
+        evens = OrdList [2, 4, 6]
+        odds = OrdList [1, 3, 5]
 
-odds :: OrdList Integer
-odds = OrdList [1,3,5]
-
-combined :: OrdList Integer
-combined = evens <> odds
-
-ordListWorks :: Bool
-ordListWorks = combined == OrdList [1,2,3,4,5,6]
-
-----------------------------------------------------
--- Exercise 6
+-- Exercise 6: write a search function given the searcher type
 
 type Searcher m = T.Text -> [Market] -> m
 
--- Search for markets with the given text in their name. Convert each found
--- market into the given Monoid m and then combine all the results with
--- `mappend` and `mconcat`.
---
--- Example usage, for Monoid [a]:
---    search (\m -> [m]) (T.pack "ell") _markets_
 search :: Monoid m => (Market -> m) -> Searcher m
-search f text = mconcat . map f . findMarketsNamedLike text
+search mk_m text = mconcat . map mk_m . filter matchesName
+    where
+        matchesName :: Market -> Bool
+        matchesName = (text `T.isInfixOf`) . marketname
 
-findMarketsNamedLike :: T.Text -> [Market] -> [Market]
-findMarketsNamedLike text = filter ((text `T.isInfixOf`) . marketname)
+-- Try it with a List monoid:
+-- markets <- loadData
+-- search (:[]) "Wednesday" markets
 
-----------------------------------------------------
--- Exercise 7
-
--- Return the first market found by a search, if any are found at all.
+-- Exercise 7: write firstFound, which finds the first result of
+-- a search, if there is one
 -- Monoid used: First
+
 firstFound :: Searcher (Maybe Market)
 firstFound text = getFirst . search (First . Just) text
 
-----------------------------------------------------
--- Exercise 8
-
--- Return the last market found by a search, if any are found at all.
+-- Exercise 8: write lastFound, which finds the last result of a search, if
+-- there is one
 -- Monoid used: Last
+
 lastFound :: Searcher (Maybe Market)
 lastFound text = getLast . search (Last . Just) text
 
-----------------------------------------------------
--- Exercise 9
+-- Exercise 9: find all markets in a search
+-- Monoid used: list (the robot monkey)
 
--- Return all markets found by a search.
--- Monoid used: [a]
 allFound :: Searcher [Market]
 allFound = search (:[])
 
-----------------------------------------------------
--- Exercise 10
+-- Exercise 10: count the number of markets found by a search
+-- Monoid used: Sum
+-- Each Market is mapped to `Sum 1`
 
--- Northern means a high y-value (Maine = 44).
--- Southern means a low y-value (Texas = 33).
-data MarketByLatitude = MarketByLatitude { market :: Market } deriving (Eq, Show)
+numberFound :: Searcher Int
+numberFound t = getSum . search (\_ -> Sum 1) t
 
-instance Ord MarketByLatitude where
-    (MarketByLatitude m1) `compare` (MarketByLatitude m2) = (y m2) `compare` (y m1)
+-- Exercise 11: orderedNtoS
+-- Search for markets, then order the results northernmost to southernmost.
 
--- Returns all markets found by a search, ordered northernmost to southernmost.
+data MarketFromNorthToSouth = MarketFromNorthToSouth { market :: Market } deriving (Show, Eq)
+
+-- y is the latitude.
+-- 0 is the Equator, and 90 is the North Pole.
+-- Therefore we sort from most-to-least y value.
+instance Ord MarketFromNorthToSouth where
+    (MarketFromNorthToSouth m1) `compare` (MarketFromNorthToSouth m2) = (y m2) `compare` (y m1)
+
 orderedNtoS :: Searcher [Market]
-orderedNtoS text = map market . getOrdList . search marketInOrdList text
-    where
-        marketInOrdList m = OrdList [MarketByLatitude m]
+orderedNtoS text = map market . getOrdList . search (\m -> OrdList [MarketFromNorthToSouth m]) text
--- fall-2014/hw6-io/Main.hs
+++ fall-2014/hw6-io/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
 import HW06
+import qualified Data.ByteString.Lazy.Char8 as B
 
-main = testParseMarkets
+main = do
+    fileData <- B.readFile "markets.json"
+    print $ fmap (take 30) $ parseMarkets fileData
