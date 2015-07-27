77d6363326a5824d7dae214562644fd41d9efc43
Gabe Berke-Williams <gabebw@gabebw.com>
2015-07-26 22:21:42 -0700

    Remove unnecessary `do`

--- fall-2014/hw3-algebraic-data-types/Main.hs
+++ fall-2014/hw3-algebraic-data-types/Main.hs
@@ -3,5 +3,4 @@ module Main where
 import Log
 import LogAnalysis
 
-main = do
-    testWhatWentWrong parse (whatWentWrongEnhanced "relish") "error.log"
+main = testWhatWentWrong parse (whatWentWrongEnhanced "relish") "error.log"
