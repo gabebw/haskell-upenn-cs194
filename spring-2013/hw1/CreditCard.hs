e2b6945052ee68bcb0892484b6d15e8cde7a9723
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-28 21:52:54 -0700

    Copy Hanoi solution to Spring 2013 HW1

--- spring-2013/hw1/CreditCard.hs
+++ spring-2013/hw1/CreditCard.hs
@@ -34,3 +34,14 @@ dividesBy10 = (== 0) . (`mod` 10)
 -- Could this number be a valid credit card number?
 validate :: Integer -> Bool
 validate = dividesBy10 . sumDigits . doubleEveryOther . toDigits
+
+-- Exercise 5: Hanoi
+
+type Peg = String
+type Move = (Peg, Peg)
+
+-- Return the list of moves to be performed to move the stack of
+-- n discs from the peg a to peg b.
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi n a b c = hanoi (n-1) a c b ++ [(a, b)] ++ hanoi (n-1) c b a
