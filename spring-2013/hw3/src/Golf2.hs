65149d9b8388f8cc1c533952557222a0fc566f17
Gabe Berke-Williams <gabebw@gabebw.com>
2015-12-03 12:46:34 -0800

    Golf2: Solve skips

--- spring-2013/hw3/src/Golf2.hs
+++ spring-2013/hw3/src/Golf2.hs
@@ -1,2 +1,17 @@
 -- http://www.seas.upenn.edu/~cis194/spring13/hw/03-rec-poly.pdf
 module Golf2 (skips, localMaxima, histogram) where
+
+skips :: [a] -> [[a]]
+skips xs = map (skips' xs) [1..(length xs)]
+
+skips' :: [a] -> Int -> [a]
+skips' xs n
+    -- If length xs < n, splitAt n will happily split it anyway, so we have to
+    -- check for that.
+    | length xs < n = []
+    | otherwise = last front : skips' back n
+        where
+            (front, back) = splitAt n xs
+
+localMaxima = undefined
+histogram = undefined
